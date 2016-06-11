fa24770d37a4c72771806aa16791d7c9401a67b2
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-06-11 10:34:54 +0700

    Document Instruction.

--- src/instruction.rs
+++ src/instruction.rs
@@ -7,15 +7,26 @@
 use std::fmt;
 use address::Address;
 
-#[allow(missing_docs)]
+/// An assembly instruction, bytecode operation, VM operation, etc.
+///
+/// This trait will be implemented for a variety of backends and
+/// provides the general means by which the rest of the code in this
+/// library can be re-used.
 pub trait Instruction: fmt::Debug {
+    /// The address of this `Instruction`.
     fn address(&self) -> Address;
 
-    fn is_local_jump(&self) -> bool;
+    /// Does this instruction terminate a `BasicBlock`?
+    fn is_block_terminator(&self) -> bool {
+        self.is_call() || self.is_local_jump() || self.is_return()
+    }
 
+    /// Does this instruction represent a call?
     fn is_call(&self) -> bool;
 
-    fn is_block_terminator(&self) -> bool {
-        self.is_local_jump() || self.is_call()
-    }
+    /// Does this instruction represent a local jump?
+    fn is_local_jump(&self) -> bool;
+
+    /// Does this instruction represent a function return?
+    fn is_return(&self) -> bool;
 }
