272c4ce800db0226036eb7f25e58095e31000f63
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-06-11 12:32:51 +0700

    Rebuild documentation at 0e5446a

--- disassemble/symbol/sidebar-items.js
+++ disassemble/symbol/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Symbol","A symbol within an executable or library. This is a named address."]]});
+initSidebarItems({"struct":[["Symbol","A symbol within an executable or library. This is a named [address]."]]});
--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['disassemble'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='disassemble/struct.Address.html' title='disassemble::Address'>Address</a>",];
+implementors['disassemble'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='disassemble/struct.Address.html' title='disassemble::Address'>Address</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='disassemble/enum.EdgeType.html' title='disassemble::EdgeType'>EdgeType</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='disassemble/enum.EdgeDirection.html' title='disassemble::EdgeDirection'>EdgeDirection</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
