a2bc0e3485383b4bea716494aa79a44e9f1ca909
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-06-13 05:51:18 +0700

    Explain what this library is in a better way.

--- README.md
+++ README.md
@@ -5,6 +5,39 @@
 
 Dual licensed under the MIT and Apache 2 licenses.
 
+This crate provides basic functionality for working with
+disassembled code. It provides (or will provide) functionality
+for performing:
+
+* Working with code that has already been compiled to machine
+  code for any CPU, bytecode, compiler IR or JIT compiler
+  output like that of V8 or the JVM. You provide an adapter
+  to teach [`Instruction`] how to report the correct information
+  for your generated code.
+* Reconstructing the [control flow graph] from a body of compiled
+  code.
+* **(Future)** Reconstructing loops and higher level control
+  flow constructs.
+* **(Future)** Performing [data flow analysis].
+* **(Future)** Generating HTML and other rich output formats
+  to assist in visualizing structure and higher level presentations
+  of the data derived from the generated code.
+* **(Future)** Writing decompilers that generate C or
+  other languages from lower level generated code.
+* **(Future)** Writing tools for reverse engineering. Many tools
+  for reverse engineering require extracting and inferring higher
+  level structure from the low level generated code. This crate
+  provides those capabilities for re-use by anyone.
+* **(Future)** Writing views that display disassembled code for
+  debuggers, profilers and other systems level tools.
+
+The actual disassembly with the implementation of [`Instruction`]
+and other elements of the system will be provided by other
+crates that integrate with other systems, such as the [Capstone
+Engine]. This crate is written such that it should work with
+nearly any machine code, VM bytecode or JIT compiler output,
+given an appropriate implementation of [`Instruction`].
+
 ## Documentation
 
 The API is fully documented with examples:
@@ -32,3 +65,8 @@ Unless you explicitly state otherwise, any contribution
 intentionally submitted for inclusion in the work by you,
 as defined in the Apache-2.0 license, shall be dual licensed
 as above, without any additional terms or conditions.
+
+[Capstone Engine]: http://www.capstone-engine.org/
+[control flow graph]: https://en.wikipedia.org/wiki/Control_flow_graph
+[data flow analysis]: https://en.wikipedia.org/wiki/Data-flow_analysis
+[`Instruction`]: https://endoli.github.io/disassemble.rs/disassemble/trait.Instruction.html
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,37 @@
 //! # Disassemble
 //!
 //! This crate provides basic functionality for working with
-//! disassembled code.
+//! disassembled code. It provides (or will provide) functionality
+//! for performing:
+//!
+//! * Working with code that has already been compiled to machine
+//!   code for any CPU, bytecode, compiler IR or JIT compiler
+//!   output like that of V8 or the JVM. You provide an adapter
+//!   to teach [`Instruction`] how to report the correct information
+//!   for your generated code.
+//! * Reconstructing the [control flow graph] from a body of compiled
+//!   code.
+//! * **(Future)** Reconstructing loops and higher level control
+//!   flow constructs.
+//! * **(Future)** Performing [data flow analysis].
+//! * **(Future)** Generating HTML and other rich output formats
+//!   to assist in visualizing structure and higher level presentations
+//!   of the data derived from the generated code.
+//! * **(Future)** Writing decompilers that generate C or
+//!   other languages from lower level generated code.
+//! * **(Future)** Writing tools for reverse engineering. Many tools
+//!   for reverse engineering require extracting and inferring higher
+//!   level structure from the low level generated code. This crate
+//!   provides those capabilities for re-use by anyone.
+//! * **(Future)** Writing views that display disassembled code for
+//!   debuggers, profilers and other systems level tools.
 //!
 //! The actual disassembly with the implementation of [`Instruction`]
 //! and other elements of the system will be provided by other
 //! crates that integrate with other systems, such as the [Capstone
-//! Engine].
+//! Engine]. This crate is written such that it should work with
+//! nearly any machine code, VM bytecode or JIT compiler output,
+//! given an appropriate implementation of [`Instruction`].
 //!
 //! It is possible (likely?) that some functionality from within
 //! this crate may move in the future to a separate crate for broader
@@ -59,6 +84,8 @@
 //!
 //! [Capstone Engine]: http://www.capstone-engine.org/
 //! [`CFG`]: struct.CFG.html
+//! [control flow graph]: https://en.wikipedia.org/wiki/Control_flow_graph
+//! [data flow analysis]: https://en.wikipedia.org/wiki/Data-flow_analysis
 //! [`Function`]: struct.Function.html
 //! [`Instruction`]: trait.Instruction.html
 //! [petgraph]: https://crates.io/crates/petgraph
