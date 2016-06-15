c98f3271d5c642d9d2b3fccfb6593c68a6a237f3
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-06-15 16:57:55 +0700

    First pass at building CFG nodes + edges.

--- src/cfg.rs
+++ src/cfg.rs
@@ -4,9 +4,11 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use basicblock::{BasicBlock, BasicBlockEdge};
+use address::Address;
+use basicblock::{BasicBlock, BasicBlockEdge, EdgeType};
 use instruction::Instruction;
 use petgraph::graph::{Graph, NodeIndex};
+use std::collections::BTreeMap;
 
 /// A [control flow graph].
 ///
@@ -21,6 +23,8 @@ pub struct CFG<'f> {
     /// [`BasicBlock`]: struct.BasicBlock.html
     /// [`NodeIndex`]: ../petgraph/graph/struct.NodeIndex.html
     pub entry_block: Option<NodeIndex>,
+    /// Map address to the corresponding basic block.
+    pub block_finder: BTreeMap<Address, NodeIndex>,
 }
 
 impl<'f> CFG<'f> {
@@ -29,15 +33,151 @@ impl<'f> CFG<'f> {
         CFG {
             graph: Graph::new(),
             entry_block: None,
+            block_finder: BTreeMap::new(),
         }
     }
+
     /// Build the CFG from the [`instructions`].
+    ///
+    /// This is conducted in a 2 step process:
+    ///
+    /// * First, each instruction is examined to identify block boundaries.
+    /// * Then, we go through each instruction again, looking for the
+    ///   previously identified block boundaries and build the edges.
+    ///
+    /// This two step process prevents us from having to construct and then
+    /// subsequently split blocks as we find backward edges.
     ///
     /// [`instructions`]: trait.Instruction.html
     pub fn build(&mut self, instructions: &Vec<Box<Instruction>>) {
-        if !instructions.is_empty() {
-            let bb = BasicBlock::new(None, instructions[0].address());
-            self.entry_block = Some(self.graph.add_node(bb));
+        if instructions.is_empty() {
+            return;
         }
+        self.identify_blocks(instructions);
+        self.build_edges(instructions);
+    }
+
+    /// Identify basic blocks by their boundaries.
+    ///
+    /// Inspect each instruction to see if it is a 'leader' or the start of
+    /// a new basic block. An instruction is a leader if:
+    ///
+    /// * It is the first instruction in the basic block.
+    /// * It comes after a branch of any sort. We identify these as things
+    ///   for which `Instruction::is_block_terminator` returns `true`.
+    /// * It is the target of a jump (conditional or unconditional) within
+    ///   the function.
+    fn identify_blocks(&mut self, instructions: &Vec<Box<Instruction>>) {
+        let start_addr = instructions[0].address();
+        let end_addr = instructions.last().map(|i| i.address()).unwrap();
+        let mut next_is_leader: bool = true;
+        for inst in instructions {
+            if next_is_leader {
+                let idx = self.graph.add_node(BasicBlock::new(None, inst.address()));
+                self.block_finder.insert(inst.address(), idx);
+                next_is_leader = false;
+            }
+            if inst.is_block_terminator() {
+                if let Some(target_addr) = inst.target_address() {
+                    if !self.block_finder.contains_key(&target_addr) &&
+                       target_addr >= start_addr && target_addr <= end_addr {
+                        let idx = self.graph.add_node(BasicBlock::new(None, target_addr));
+                        self.block_finder.insert(target_addr, idx);
+                    }
+                }
+                // The next instruction, if any, will be the start of a new block.
+                next_is_leader = true;
+            }
+        }
+        self.entry_block = Some(self.block_finder.get(&instructions[0].address()).unwrap().clone());
+    }
+
+    /// Build an edge between 2 basic blocks.
+    fn build_edge(&mut self,
+                  current_block_idx: NodeIndex,
+                  next_block_idx: NodeIndex,
+                  current_inst: &Box<Instruction>) {
+        if current_inst.is_local_conditional_jump() {
+            // We have one edge for the jump target and one for the fallthrough.
+            // We jump through some hoops here to keep the borrow checker happy.
+            if let Some(target_addr) = current_inst.target_address() {
+                if let Some(target_block_idx) = self.block_finder.get(&target_addr) {
+                    let edge = BasicBlockEdge { edge_type: EdgeType::ConditionalTaken };
+                    self.graph.add_edge(current_block_idx, target_block_idx.clone(), edge);
+                }
+                let edge = BasicBlockEdge { edge_type: EdgeType::ConditionalFallthrough };
+                self.graph.add_edge(current_block_idx, next_block_idx, edge);
+            }
+        } else if current_inst.is_local_jump() || current_inst.is_call() {
+            let edge = BasicBlockEdge { edge_type: EdgeType::Unconditional };
+            self.graph.add_edge(current_block_idx, next_block_idx, edge);
+        } else if current_inst.is_return() {
+            // Do we want to record this exit anywhere?
+        }
+    }
+
+    /// Build edges after having identified the basic blocks.
+    ///
+    /// After the basic blocks have been identified, we can go back and
+    /// connect the basic blocks with their appropriate edges.
+    ///
+    /// We do this by iterating through the instructions looking for
+    /// boundaries between the basic blocks and then setting up the
+    /// new edges.
+    fn build_edges(&mut self, instructions: &Vec<Box<Instruction>>) {
+        // Here, we're going to walk through the instructions again in
+        // pairs of (i, Some(i + 1)):
+        // [1,2..9] -> [Some(1),Some(2)..Some(9),None]
+        let current_inst_iter = instructions.iter();
+        let mut next_inst_iter = current_inst_iter.clone().map(Some).chain(Some(None).into_iter());
+        // Skip the first one so that we're actually working with the next one.
+        next_inst_iter.next();
+        let mut current_block_idx = self.entry_block.unwrap();
+        for ij in current_inst_iter.zip(next_inst_iter) {
+            if let (current_inst, Some(next_inst)) = ij {
+                // Does the next instruction begin a basic block?
+                let next_block_idx = self.block_finder
+                    .get(&next_inst.address())
+                    .unwrap_or(&current_block_idx)
+                    .clone();
+                // If we're at a block boundary, create an edge between the
+                // current and next blocks. The type of the edge is determined
+                // by looking at the current instruction.
+                if next_block_idx != current_block_idx {
+                    self.build_edge(current_block_idx, next_block_idx, current_inst);
+                    current_block_idx = next_block_idx;
+                }
+            } else {
+                // No next instruction, so we're at the end.
+                // Do we want to record this exit anywhere?
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use instruction::Instruction;
+    use super::CFG;
+    use tests::*;
+
+    #[test]
+    fn construct() {
+        let cfg = CFG::new();
+        assert!(cfg.entry_block.is_none());
+        assert_eq!(cfg.graph.node_count(), 0);
+    }
+
+    #[test]
+    fn build_one_basic_block() {
+        let mut cfg = CFG::new();
+        let insts: Vec<Box<Instruction>> = vec![
+            TestInstruction::new(0, Opcode::Add),
+            TestInstruction::new(1, Opcode::Add),
+            TestInstruction::new(2, Opcode::Ret),
+        ];
+        cfg.build(&insts);
+        assert!(cfg.entry_block.is_some());
+        assert_eq!(cfg.graph.node_count(), 1);
     }
 }
